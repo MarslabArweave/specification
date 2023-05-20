c8e7c08f24fbbe2e11eb20abc7a24a13ce2e16c0
mARsLab <marslab.2022@gmail.com>
2023-05-20 17:34:35 +0800

    feat: faces support atomic nft/token

--- contracts/atomic-asset-typescript/lib/faces.ts
+++ contracts/atomic-asset-typescript/lib/faces.ts
@@ -2,6 +2,8 @@ export interface AtomicAssetState {
     name?: string;
     description?: string;
     owner?: string;
+    logo?: string; // transaction id. for atomic token only
+    collection?: string; //transaction id. for atomic nft only
     symbol: string;
     decimals: number;
     totalSupply: number;
